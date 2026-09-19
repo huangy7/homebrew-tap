7f0049283344b181bdf5a51c51e96d7b83056557
huangy7 <54624301+huangy7@users.noreply.github.com>
2026-09-19 19:15:48 +0800

    fix: 下载地址改用固定命名资产（MeowOut-arm64.dmg / x86_64.dmg）

--- Casks/meowout.rb
+++ Casks/meowout.rb
@@ -3,10 +3,10 @@ cask "meowout" do
   
   if Hardware::CPU.intel?
     sha256 "3f1e6725837d0345ab58c57e0f4907ee26880cf5ab7e9fe83f449c50922af638"
-    url "https://github.com/huangy7/MeowOut/releases/download/v#{version}/MeowOut-#{version}-x86_64.dmg"
+    url "https://github.com/huangy7/MeowOut/releases/download/v#{version}/MeowOut-x86_64.dmg"
   else
     sha256 "1e53bdbc8316b9dc7fdfed256dbaa1e9168d7867da2270047a3ebcb46fbfa543"
-    url "https://github.com/huangy7/MeowOut/releases/download/v#{version}/MeowOut-#{version}-arm64.dmg"
+    url "https://github.com/huangy7/MeowOut/releases/download/v#{version}/MeowOut-arm64.dmg"
   end
 
   name "MeowOut"
